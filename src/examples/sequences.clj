cdb6b6a79819aaf37e4c2426f69ae5a77efaa884
emag <lanabe.lanabe@gmail.com>
2015-12-28 17:00:51 +0900

    Done 73/244

--- src/examples/sequences.clj
+++ src/examples/sequences.clj
@@ -13,3 +13,18 @@
 (defn recently-modified? [file]
   (> (.lastModified file)
      (- (System/currentTimeMillis) (minutes-to-millis 30))))
+
+(:use '[clojure.java.io :only (reader)])
+(defn non-blank? [line] (if (re-find #"\S" line) true false))
+
+(defn non-git? [file] (not (.contains (.toString file) ".git")))
+
+(defn clojure-source? [file] (.endsWith (.toString file) ".clj"))
+
+(defn clojure-loc [base-file]
+  (reduce
+    +
+    (for [file (file-seq base-file)
+          :when (and (clojure-source? file) (non-git? file))]
+      (with-open [rdr (reader file)]
+        (count (filter non-blank? (line-seq rdr)))))))
