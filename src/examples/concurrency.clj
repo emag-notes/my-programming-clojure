e630247947555534b47a44a7a2b535c44f1d6544
emag <lanabe.lanabe@gmail.com>
2015-12-31 22:22:20 +0900

    Done 134/244

--- src/examples/concurrency.clj
+++ src/examples/concurrency.clj
@@ -1,5 +1,31 @@
-(ns examples.concurrency)
+(ns examples.concurrency
+  (:use examples.chat))
 
 (def counter (ref 0))
 
 (defn next-counter [] (dosync (alter counter inc)))
+
+(def counter (agent 0 :validator number?))
+
+(def backup-agent (agent "output/messages-backup.clj"))
+
+(defn add-message-with-backup [msg]
+  (dosync
+    (let [snapshot (commute messages conj msg)]
+      (send-off backup-agent (fn [filename]
+                               (spit filename snapshot)
+                               filename))
+      snapshot)))
+
+(defn ^:dynamic slow-double [n]
+  (Thread/sleep 100)
+  (* n 2))
+
+(defn calls-slow-double []
+  (map slow-double [1 2 1 2 1 2]))
+
+(defn demo-memoize []
+  (time
+    (dorun
+      (binding [slow-double (memoize slow-double)]
+        (calls-slow-double)))))
---
+++ src/reader/snake.clj
@@ -0,0 +1,6 @@
+(ns reader.snake
+  (:import (java.awt Color Dimension)
+           (javax.swing JPanel JFrame Timer JOptionPane)
+           (java.awt.event ActionListener KeyListener))
+  (:use examples.import-static))
+(import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
