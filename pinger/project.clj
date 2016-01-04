2acc7cff47f0481335ea915247df02854ef2a151
emag <lanabe.lanabe@gmail.com>
2016-01-05 02:25:46 +0900

    Done 220/244

--- pinger/project.clj
+++ pinger/project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j "1.2.17"]
+                 [javax.mail/mail "1.4.7"]]
   :main pinger.core)
--- pinger/src/pinger/core.clj
+++ pinger/src/pinger/core.clj
@@ -1,5 +1,8 @@
 (ns pinger.core
   (:import (java.net URL HttpURLConnection))
+  (:require [pinger.scheduler :as scheduler]
+            [pinger.config :as config]
+            [clojure.tools.logging :as logger])
   (:gen-class))
 
 (defn response-code [address]
@@ -12,11 +15,28 @@
 (defn available? [address]
   (= 200 (response-code address)))
 
+(defn record-availability [address]
+  (if (available? address)
+    (logger/info (str address " is responding normally"))
+    (logger/error (str address " is not available"))))
+
+(defn check []
+  (let [addresses (config/urls (config/config))]
+    (doseq [address addresses]
+      (record-availability address))))
+
+(def immediately 0)
+(def every-minute (* 60 1000))
+
+(defn start [e]
+  "REPL helper. Start pinger on executor e."
+  (scheduler/periodically e check
+                          :initial-delay immediately
+                          :delay every-minute))
+
+(defn stop [e]
+  "REPL helper. Stop executor e."
+  (scheduler/shutdown-executor e))
+
 (defn -main []
-  (let [addresses '("http://google.com"
-                     "http://amazon.com"
-                     "http://google.com/badurl")]
-    (while true
-      (doseq [address addresses]
-        (println (available? address)))
-      (Thread/sleep (* 1000 60)))))
+  (start (scheduler/scheduled-executor 1)))
