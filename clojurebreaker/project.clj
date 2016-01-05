86f52c8225e32b98185827e607fb5ebcee5efb5a
emag <lanabe.lanabe@gmail.com>
2016-01-06 01:34:32 +0900

    Done 244/244

--- clojurebreaker/project.clj
+++ clojurebreaker/project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.0.1"]
-                 [org.clojure/test.generative "0.1.3"]])
+                 [org.clojure/test.generative "0.1.3"]
+                 [noir "1.3.0-beta3"]]
+  :main clojurebreaker.server)
